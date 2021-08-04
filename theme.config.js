332a260eda451e53c4f5d42416fa8078405937d0
jk <72466440+jk10274@users.noreply.github.com>
2021-08-04 18:21:00 +0200

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,11 +1,11 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/apuem/ask',
+  titleSuffix: ' – ask.apuem',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">ask.apuem</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Site Builder
+        The General Knowledge Project.
       </span>
     </>
   ),
@@ -15,15 +15,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="ask.apuem: Learn everyday something new!" />
+      <meta name="og:description" content="ask.apuem: Learn everyday something new!" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="https://a-static.github.io/a.png" />
+      <meta name="twitter:site:domain" content="ask.apuem.com" />
+      <meta name="twitter:url" content="https://ask.apuem.com" />
+      <meta name="og:title" content="ask.apuem: Learn everyday something new!" />
+      <meta name="og:image" content="https://a-static.github.io/a.png" />
+      <meta name="apple-mobile-web-app-title" content="ask.apuem" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -61,5 +61,5 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditOnGitHubLink: true,
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>Powered by apuem.</>,
 }
